4c46d0d3da07e927c76c7b640173fd4b9409a75e
Ricky_Zeng <zengchiwai08@gmail.com>
2025-07-10 14:33:48 +0800

    finish input and display digit logic

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let num1 , operator , num2 ;
+const buttonContainer = document.querySelector(".button-container");
+const display = document.querySelector(".display");
+let input_buffer = "";
+let cal_array = [0,0,0];
 
 function add(a,b){
     return a+b;
@@ -22,3 +26,28 @@ function operate(firstNum,oneOperator,secondNum){
     else if(oneOperator == "*") return multiply(firstNum,secondNum);
     else if(oneOperator == "/") return divide(firstNum,secondNum);
 }
+
+function updateBufferAndDisplay(event){
+    input_buffer += event.target.textContent;
+    display.textContent = input_buffer;
+}
+
+//make sure the display will not overflow by limiting the input
+function isValidDisplayLength(){
+    if(display.textContent.length < 16) return true;
+    return false;
+}
+
+function main(event){
+    let buttonType = event.target.classList.value;
+    if(buttonType == "digit"){
+        if(isValidDisplayLength()) updateBufferAndDisplay(event);
+        
+    }
+    else if(buttonType == "divides" || buttonType == "times" || buttonType == "adds" || buttonType == "subtracts"){
+
+    }
+    console.log(buttonType, typeof buttonType)
+}
+
+buttonContainer.addEventListener("click",main);
